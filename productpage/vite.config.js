fc416924dc9b0426751edcad28db892c0600833f
Prerit Nag <preritnag4@gmail.com>
2026-02-05 17:04:30 +0530

    go

--- productpage/vite.config.js
+++ productpage/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: "/",          // 👈 ADD THIS
-  plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
-})
+export default defineConfig({       
+    base: "./",
+    plugins: [react()],
+    build: {
+      outDir: 'dist'
+    }
+  })
