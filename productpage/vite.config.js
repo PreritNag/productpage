496676301732514f313d1c1412ce3db163d1528e
Prerit Nag <preritnag4@gmail.com>
2026-02-05 16:36:29 +0530

    go

--- productpage/vite.config.js
+++ productpage/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  
+  define: {
+    global: 'window',
+  },
+  build: {
+    outDir: 'dist' 
+  }
 })
