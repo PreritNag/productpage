9829851f53b652d07355c08f65d4a202827e3860
Prerit Nag <preritnag4@gmail.com>
2026-02-05 16:46:47 +0530

    Bankai

--- productpage/vite.config.js
+++ productpage/vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/",          // 👈 ADD THIS
   plugins: [react()],
-  define: {
-    global: 'window',
-  },
   build: {
     outDir: 'dist'
   }
